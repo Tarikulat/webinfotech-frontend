76d63d9e1640e3d28a14ccfce46dd68107543f84
Tarikulat <tarikulat124@gmail.com>
2024-07-09 12:06:54 +0600

    working on animation button

--- src/views/common/HomeSection.vue
+++ src/views/common/HomeSection.vue
@@ -22,37 +22,55 @@
                                             our customers' expectations with the help of experience combined with
                                             state-of-art techniques and technologies.
                                         </p>
-                                        <!-- <ul class="list-unstyled tech-feature-list text-white">
-                                                <li class="py-1"><span class="ti-control-forward mr-2"></span>Flexible,
-                                                    Easy
-                                                    to Use Control Panel</li>
-                                                <li class="py-1"><span
-                                                        class="ti-control-forward mr-2"></span><strong>Unmetered</strong>
-                                                    Bandwidth</li>
-                                                <li class="py-1"><span
-                                                        class="ti-control-forward mr-2"></span><strong>Unmetered</strong>
-                                                    Sub Domains, FTP Accounts</li>
-                                                <li class="py-1"><span
-                                                        class="ti-control-forward mr-2"></span><strong>99%</strong>
-                                                    Uptime
-                                                    Guarantee</li>
-                                                <li class="py-1"><span
-                                                        class="ti-control-forward mr-2"></span><strong>45-Day</strong>
-                                                    Money-Back Guarantee</li>
-                                            </ul> -->
-                                        <!-- <div class="action-btns mt-4">
-                                                <p>Get all the essentials features <strong>Starting at $2.59/mo</strong>
-                                                </p>
-                                                <a href="#" class="btn solid-white-btn mr-3">Check Prices</a>
-                                                <a href="#" class="btn outline-white-btn">Learn More</a>
-                                            </div> -->
                                     </div>
+
+                                    <div class="domain-list-wrap mt-4">
+                                        <ul class="list-inline domain-search-list text-white">
+                                            <li class="list-inline-item"><a href="#"><img src="@/assets/img/hi1.png"
+                                                        alt="com" width="70" class="img-fluid" />
+                                                </a></li>
+                                            <li class="list-inline-item"><a href="#"><img src="@/assets/img/hi2.png"
+                                                        alt="com" width="70" class="img-fluid" />
+                                                </a></li>
+                                            <li class="list-inline-item"><a href="#"><img src="@/assets/img/hi3.png"
+                                                        alt="com" width="70" class="img-fluid" />
+                                                </a></li>
+                                        </ul>
+                                    </div> <br>
+                                    
+                                           <ul class="">
+                                            <li><button class="btnb" type="button">
+                                                    <strong>What We Do</strong>
+                                                    <div id="container-stars">
+                                                        <div id="stars"></div>
+                                                    </div>
+
+                                                    <div id="glow">
+                                                        <div class="circle"></div>
+                                                        <div class="circle"></div>
+                                                    </div>
+                                                </button></li> <br>
+
+                                            <li> <button class="button">
+                                                    <span class="span"><svg xmlns="http://www.w3.org/2000/svg"
+                                                            fill="none" viewBox="0 0 23 21" height="21" width="23"
+                                                            class="svg-icon">
+                                                            <path stroke-linejoin="round" stroke-linecap="round"
+                                                                stroke-width="2" stroke="black"
+                                                                d="M1.97742 19.7776C4.45061 17.1544 7.80838 15.5423 11.5068 15.5423C15.2053 15.5423 18.5631 17.1544 21.0362 19.7776M16.2715 6.54229C16.2715 9.17377 14.1383 11.307 11.5068 11.307C8.87535 11.307 6.74212 9.17377 6.74212 6.54229C6.74212 3.91082 8.87535 1.77759 11.5068 1.77759C14.1383 1.77759 16.2715 3.91082 16.2715 6.54229Z">
+                                                            </path>
+                                                        </svg></span>
+                                                    <span class="lable">Details</span>
+                                                </button></li>
+                                        </ul>
+                                  
+
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="item">
+                <!-- <div class="item">
                     <div class="gradient-overly-right hero-equal-height ptb-100"
                         style="background: url('@/assets/img/hero-10.jpg')no-repeat center center / cover">
                         <div class="container">
@@ -76,24 +94,15 @@
                                         </form>
                                         <div class="domain-list-wrap mt-4">
                                             <ul class="list-inline domain-search-list text-white">
-                                                <li class="list-inline-item"><a href="#"><img
-                                                            src="@/assets/img/com-w.png" alt="com" width="70"
-                                                            class="img-fluid" />
-                                                        <span>$8.99</span></a></li>
-                                                <li class="list-inline-item"><a href="#"><img
-                                                            src="@/assets/img/net-w.png" alt="com" width="70"
-                                                            class="img-fluid" />
-                                                        <span>$4.99</span></a></li>
-                                                <li class="list-inline-item"><a href="#"><img
-                                                            src="@/assets/img/org-w.png" alt="com" width="70"
-                                                            class="img-fluid" />
-                                                        <span>$2.99</span></a></li>
-                                                <li class="list-inline-item"><a href="#"><img
-                                                            src="@/assets/img/store-w.png" alt="com" width="70"
-                                                            class="img-fluid" /> <span>$0.99</span></a></li>
-                                                <li class="list-inline-item"><a href="#"><img
-                                                            src="@/assets/img/online-w.png" alt="com" width="70"
-                                                            class="img-fluid" /> <span>$0.99</span></a></li>
+                                                <li class="list-inline-item"><a href="#"><img src="@/assets/img/hi1.png"
+                                                            alt="com" width="70" class="img-fluid" />
+                                                    </a></li>
+                                                <li class="list-inline-item"><a href="#"><img src="@/assets/img/hi2.png"
+                                                            alt="com" width="70" class="img-fluid" />
+                                                    </a></li>
+                                                <li class="list-inline-item"><a href="#"><img src="@/assets/img/hi3.png"
+                                                            alt="com" width="70" class="img-fluid" />
+                                                    </a></li>
                                             </ul>
                                         </div>
                                     </div>
@@ -123,7 +132,7 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </div>
         </section>
         <!--hero section end-->
@@ -1081,7 +1090,227 @@
     background-image: url('@/assets/img/home1.jpg');
     height: 700px;
 } */
+ .btnb {
+     display: flex;
+     justify-content: center;
+     align-items: center;
+     width: 13rem;
+     overflow: hidden;
+     height: 3rem;
+     background-size: 300% 300%;
+     backdrop-filter: blur(1rem);
+     border-radius: 5rem;
+     transition: 0.5s;
+     animation: gradient_301 5s ease infinite;
+     border: double 4px transparent;
+     background-image: linear-gradient(#212121, #212121), linear-gradient(137.48deg, #ffdb3b 10%, #FE53BB 45%, #8F51EA 67%, #0044ff 87%);
+     background-origin: border-box;
+     background-clip: content-box, border-box;
+ }
 
+ #container-stars {
+     position: absolute;
+     z-index: -1;
+     width: 100%;
+     height: 100%;
+     overflow: hidden;
+     transition: 0.5s;
+     backdrop-filter: blur(1rem);
+     border-radius: 5rem;
+ }
+
+ strong {
+     z-index: 2;
+     font-family: 'Avalors Personal Use';
+     font-size: 12px;
+     letter-spacing: 5px;
+     color: #FFFFFF;
+     text-shadow: 0 0 4px white;
+ }
+
+ #glow {
+     position: absolute;
+     display: flex;
+     width: 12rem;
+ }
+
+ .circle {
+     width: 100%;
+     height: 30px;
+     filter: blur(2rem);
+     animation: pulse_3011 4s infinite;
+     z-index: -1;
+ }
+
+ .circle:nth-of-type(1) {
+     background: rgba(254, 83, 186, 0.636);
+ }
+
+ .circle:nth-of-type(2) {
+     background: rgba(142, 81, 234, 0.704);
+ }
+
+ .btn:hover #container-stars {
+     z-index: 1;
+     background-color: #212121;
+ }
+
+ .btn:hover {
+     transform: scale(1.1)
+ }
+
+ .btn:active {
+     border: double 4px #FE53BB;
+     background-origin: border-box;
+     background-clip: content-box, border-box;
+     animation: none;
+ }
+
+ .btn:active .circle {
+     background: #FE53BB;
+ }
+
+ #stars {
+     position: relative;
+     background: transparent;
+     width: 200rem;
+     height: 200rem;
+ }
+
+ #stars::after {
+     content: "";
+     position: absolute;
+     top: -10rem;
+     left: -100rem;
+     width: 100%;
+     height: 100%;
+     animation: animStarRotate 90s linear infinite;
+ }
+
+ #stars::after {
+     background-image: radial-gradient(#ffffff 1px, transparent 1%);
+     background-size: 50px 50px;
+ }
+
+ #stars::before {
+     content: "";
+     position: absolute;
+     top: 0;
+     left: -50%;
+     width: 170%;
+     height: 500%;
+     animation: animStar 60s linear infinite;
+ }
+
+ #stars::before {
+     background-image: radial-gradient(#ffffff 1px, transparent 1%);
+     background-size: 50px 50px;
+     opacity: 0.5;
+ }
+
+ @keyframes animStar {
+     from {
+         transform: translateY(0);
+     }
+
+     to {
+         transform: translateY(-135rem);
+     }
+ }
+
+ @keyframes animStarRotate {
+     from {
+         transform: rotate(360deg);
+     }
+
+     to {
+         transform: rotate(0);
+     }
+ }
+
+ @keyframes gradient_301 {
+     0% {
+         background-position: 0% 50%;
+     }
+
+     50% {
+         background-position: 100% 50%;
+     }
+
+     100% {
+         background-position: 0% 50%;
+     }
+ }
+
+ @keyframes pulse_3011 {
+     0% {
+         transform: scale(0.75);
+         box-shadow: 0 0 0 0 rgba(0, 0, 0, 0.7);
+     }
+
+     70% {
+         transform: scale(1);
+         box-shadow: 0 0 0 10px rgba(0, 0, 0, 0);
+     }
+
+     100% {
+         transform: scale(0.75);
+         box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+     }
+ }
+
+ /* section 1 of 2nd btn from tarikul */
+ .button {
+     display: flex;
+     justify-content: center;
+     align-items: center;
+     padding: 9px 12px;
+     gap: 8px;
+     height: 45px;
+     width: 150px;
+     border: none;
+     background: rgb(74, 180, 50);
+     border-radius: 50px;
+     cursor: pointer;
+     position: relative;
+ }
+
+ .lable {
+     line-height: 22px;
+     font-size: 17px;
+     color: #fff;
+     margin-left: 20px;
+     font-family: sans-serif;
+     letter-spacing: 1px;
+ }
+
+ .button .span {
+     border-radius: 50%;
+     background-color: rgb(48, 129, 29);
+     padding: 10px;
+     position: absolute;
+     left: 0;
+ }
+
+ .button:hover {
+     background: rgb(48, 129, 29);
+ }
+
+ .button:hover .svg-icon {
+     animation: slope 0.8s linear infinite;
+ }
+
+ @keyframes slope {
+     0% {}
+
+     50% {
+         transform: rotate(15deg);
+     }
+
+     100% {}
+ }
+
+ /* section 2 btn from tarikul */
  .button {
      position: relative;
      transition: all 0.3s ease-in-out;
