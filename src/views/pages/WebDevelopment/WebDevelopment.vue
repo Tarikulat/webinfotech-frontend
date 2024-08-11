b52a0c606c86af8424d5fe1fc1d55b15c0be0881
Tarikulat <tarikulat124@gmail.com>
2024-08-11 18:38:31 +0600

    update section

--- src/views/pages/WebDevelopment/WebDevelopment.vue
+++ src/views/pages/WebDevelopment/WebDevelopment.vue
@@ -572,17 +572,201 @@
                   </div>
                </div>
             </div>
-            <div class="col-12">
-               <div class="support-cta mt-5">
-                  <h5 class="mb-1 d-flex align-items-center justify-content-center"><span
-                        class="ti-loop color-primary mr-3 icon-sm"></span>Choose Your <a href="pricing-comparison.html"
-                        class="ml-2">Compare Hosting Plans</a></h5>
+         </div>
+      </div>
+   </section>
+   <!--pricing with switch section end-->
+
+   <!--support category section start-->
+   <section class="support-category ptb-100">
+      <div class="container">
+         <div class="row justify-content-center">
+
+            <div class="col-md-9 col-lg-8">
+               <div class="section-heading mb-5 text-center">
+                  <h2>How we Define our Support Services</h2>
+                  <p class="lead">Distinctively productize team driven niches with</p>
+               </div>
+            </div>
+         </div>
+
+         <div class="row justify-content-around">
+            <div class="col-md-6 col-lg-6">
+               <div class="help-center-left">
+                  <ul class="list-unstyled popular-categories-list">
+                     <li class="d-flex align-items-center border rounded py-4 px-4">
+                        <div class="icon-wrap mr-4">
+                           <span class="ti-star icon-sm color-5 color-5-bg p-3 rounded"></span>
+                        </div>
+                        <div class="help-center-info">
+                           <h5>How we Define our Support Services</h5>
+                           <p class="mb-0">Rapidiously reconceptualize technically
+                              professional. </p>
+                           <a class="help-center-box-link" href="#"><i
+                                 class="ti-angle-right p-3 rounded-circle primary-bg text-white"></i></a>
+                        </div>
+                     </li>
+                     <li class="d-flex align-items-center border rounded py-4 px-4">
+                        <div class="icon-wrap mr-4">
+                           <span class="ti-star icon-sm color-3 color-3-bg p-3 rounded"></span>
+                        </div>
+                        <div class="help-center-info">
+                           <h5>How we Define our Support Services</h5>
+                           <p class="mb-0">Rapidiously reconceptualize technically
+                              professional. </p>
+                           <a class="help-center-box-link" href="#"><i
+                                 class="ti-angle-right p-3 rounded-circle primary-bg text-white"></i></a>
+                        </div>
+                     </li>
+                     <li class="d-flex align-items-center border rounded py-4 px-4">
+                        <div class="icon-wrap mr-4">
+                           <span class="ti-star icon-sm color-4 color-4-bg p-3 rounded"></span>
+                        </div>
+                        <div class="help-center-info">
+                           <h5 class="mb-0">Payment issue</h5>
+                           <p class="mb-0">Intrinsicly iterate functional customer
+                              systems. </p>
+                           <a class="help-center-box-link" href="#"><i
+                                 class="ti-angle-right p-3 rounded-circle primary-bg text-white"></i></a>
+                        </div>
+                     </li>
+                     <li class="d-flex align-items-center border rounded py-4 px-4">
+                        <div class="icon-wrap mr-4">
+                           <span class="ti-star icon-sm color-4 color-4-bg p-3 rounded"></span>
+                        </div>
+                        <div class="help-center-info">
+                           <h5 class="mb-0">Payment issue</h5>
+                           <p class="mb-0">Intrinsicly iterate functional customer
+                              systems. </p>
+                           <a class="help-center-box-link" href="#"><i
+                                 class="ti-angle-right p-3 rounded-circle primary-bg text-white"></i></a>
+                        </div>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+            <div class="col-md-6 col-lg-6">
+               <div class="help-center-left">
+                  <ul class="list-unstyled popular-categories-list">
+                     <li class="d-flex align-items-center border rounded py-4 px-4">
+                        <div class="icon-wrap mr-4">
+                           <span class="ti-star icon-sm color-5 color-5-bg p-3 rounded"></span>
+                        </div>
+                        <div class="help-center-info">
+                           <h5 class="mb-0">Our servers status</h5>
+                           <p class="mb-0">Rapidiously reconceptualize
+                              professional. </p>
+                           <a class="help-center-box-link" href="#"><i
+                                 class="ti-angle-right p-3 rounded-circle primary-bg text-white"></i></a>
+                        </div>
+                     </li>
+                     <li class="d-flex align-items-center border rounded py-4 px-4">
+                        <div class="icon-wrap mr-4">
+                           <span class="ti-star icon-sm color-3 color-3-bg p-3 rounded"></span>
+                        </div>
+                        <div class="help-center-info">
+                           <h5 class="mb-0">Our domains services</h5>
+                           <p class="mb-0">Efficiently target tactical growth
+                              e-commerce. </p>
+                           <a class="help-center-box-link" href="#"><i
+                                 class="ti-angle-right p-3 rounded-circle primary-bg text-white"></i></a>
+                        </div>
+                     </li>
+                     <li class="d-flex align-items-center border rounded py-4 px-4">
+                        <div class="icon-wrap mr-4">
+                           <span class="ti-star icon-sm color-4 color-4-bg p-3 rounded"></span>
+                        </div>
+                        <div class="help-center-info">
+                           <h5 class="mb-0">Payment issue</h5>
+                           <p class="mb-0">Intrinsicly iterate functional customer
+                              systems. </p>
+                           <a class="help-center-box-link" href="#"><i
+                                 class="ti-angle-right p-3 rounded-circle primary-bg text-white"></i></a>
+                        </div>
+                     </li>
+                     <li class="d-flex align-items-center border rounded py-4 px-4">
+                        <div class="icon-wrap mr-4">
+                           <span class="ti-star icon-sm color-4 color-4-bg p-3 rounded"></span>
+                        </div>
+                        <div class="help-center-info">
+                           <h5 class="mb-0">Payment issue</h5>
+                           <p class="mb-0">Intrinsicly iterate functional customer
+                              systems. </p>
+                           <a class="help-center-box-link" href="#"><i
+                                 class="ti-angle-right p-3 rounded-circle primary-bg text-white"></i></a>
+                        </div>
+                     </li>
+                  </ul>
                </div>
             </div>
          </div>
       </div>
    </section>
-   <!--pricing with switch section end-->
+   <!--support category section end-->
+
+   <!--faq new style start-->
+   <section class="ptb-100 gray-light-bg">
+      <div class="container">
+         <div class="row justify-content-center">
+            <div class="col-md-9 col-lg-8">
+               <div class="section-heading mb-3 text-center">
+                  <h2>Frequently Asked Questions</h2>
+                  <p class="lead">
+                     Quickly morph client-centric results through performance based applications. Proactively facilitate
+                     professional human capital for cutting-edge.
+                  </p>
+               </div>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-md-6">
+               <div class="single-faq pt-4">
+                  <h5>How can I pay for this?</h5>
+                  <p>Intrinsicly implement high standards in strategic theme areas via inexpensive solutions.
+                     Assertively conceptualize prospective bandwidth whereas client-based imperatives.</p>
+               </div>
+            </div>
+            <div class="col-md-6">
+               <div class="single-faq pt-4">
+                  <h5>Is it possible to pay yearly?</h5>
+                  <p>Assertively iterate user friendly innovation without open-source markets. Monotonectally extend
+                     resource sucking manufactured products without high-payoff paradigms. Objectively customize
+                     ubiquitous information before economically sound relationships. </p>
+               </div>
+            </div>
+            <div class="col-md-6">
+               <div class="single-faq pt-4">
+                  <h5>Do you offer discounts on multiple items?</h5>
+                  <p>Dramatically target focused testing procedures after holistic ideas. Collaboratively maximize
+                     high-payoff ROI and value-added products. Distinctively deliver cooperative collaboration and
+                     idea-sharing whereas customized</p>
+               </div>
+            </div>
+            <div class="col-md-6">
+               <div class="single-faq pt-4">
+                  <h5>Is VAT included in plan prices?</h5>
+                  <p>Distinctively simplify high-quality initiatives for highly efficient applications. Monotonectally
+                     repurpose integrated customer service after magnetic e-services. </p>
+               </div>
+            </div>
+            <div class="col-md-6">
+               <div class="single-faq pt-4">
+                  <h5>Will I pay more for some features?</h5>
+                  <p>Enthusiastically pontificate resource-leveling supply chains whereas scalable markets.
+                     Authoritatively streamline resource maximizing methods of empowerment</p>
+               </div>
+            </div>
+            <div class="col-md-6">
+               <div class="single-faq pt-4">
+                  <h5>Why are there no limits on the number of messages?</h5>
+                  <p>Assertively target turnkey ideas for market-driven portals. Appropriately e-enable world-class
+                     intellectual capital whereas 2.0 mindshare.</p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+   <!--faq new style end-->
 
 </template>
 
